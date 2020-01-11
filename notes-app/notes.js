a474b5b060075cd590a9a7f10c75b5019213feb4
AlexKovalchuk <songossj372@gmail.com>
2020-01-11 16:36:24 +0200

    init

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -23,7 +23,7 @@ const listNotes = () => {
 const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNote = notes.find(note => note.title === title);
-
+  debugger;
   if(!duplicateNote) {
     notes.push({title: title, body: body});
     saveNotes(notes);
