df0ad75605f7a85a898aba3ae416b7cf5f86789f
AlexKovalchuk <songossj372@gmail.com>
2020-01-28 10:48:47 +0200

    beauty

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,6 @@
-import { join } from 'path'
-import express, { static } from 'express'
-import { registerPartials } from 'hbs'
+import {join} from 'path'
+import express, {static} from 'express'
+import {registerPartials} from 'hbs'
 import geocode from './utils/geocode'
 import forecast from './utils/forecast'
 
@@ -43,41 +43,31 @@ app.get('/help', (req, res) => {
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
+        return res.send({error: 'You must provide an address!'})
     }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if (error) {
-            return res.send({ error })
+            return res.send({error})
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
-                return res.send({ error })
+                return res.send({error})
             }
 
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
+            res.send({forecast: forecastData, location, address: req.query.address})
         })
     })
 })
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
+        return res.send({error: 'You must provide a search term'})
     }
 
     console.log(req.query.search)
-    res.send({
-        products: []
-    })
+    res.send({products: []})
 })
 
 app.get('/help/*', (req, res) => {
